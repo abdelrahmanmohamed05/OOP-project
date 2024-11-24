f07bc8725df88932f78a5fb2ed53c7b04cad25db
abdelrahman mohamed <boodimohamed.2005@gmail.com>
2024-11-24 21:34:12 +0200

    update the project

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello");
-        System.out.println("make a new update");
-        System.out.println("new push");
-        System.out.println();
+
     }
 }
